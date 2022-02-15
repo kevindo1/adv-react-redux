0b0df65a154719e0c5603b7979f5c4c635588294
Kevin Do <kevind0501@gmail.com>
2022-02-15 11:28:01 -0800

    created slice of state

---
+++ redux-spotlight/src/views/Counter/counterSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialCount = {
+  count: 0,
+};
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialCount,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+  },
+});
